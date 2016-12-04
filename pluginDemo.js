bb7863d15120361890abd05a18bac44c4ecb584f
spacewei <693906369@qq.com>
2016-12-04 23:00:39 +0800

    20161204
    日历插件面对对象化尝试,禁止手机上缩放

---
+++ pluginDemo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by SPACEY on 2016/12/3.
+ */
+$('.calendar-space-toggle').on('click',function(e){
+    //$('body').get(0).innerHTML += '<div class="calendar-space"></div>';
+    //$('.show').get(0).innerHTML += '<div class="calendar-space"></div>';
+    //$('body').get(0).innerHTML += '<script src="calendarSpace.js">'+'</script>';
+    //calendarSpace.calendarType(1);
+    //var select = calendarSpace.calendarType();
+    //select(0);
+    //$(e.target '.')
+});
+//$()
